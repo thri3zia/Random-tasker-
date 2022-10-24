2d9bef01eba0cdff7b6ce7be9fbe03bdba2b8cfe
codewithjuspher <21103857@usc.edu.ph>
2022-10-24 14:54:40 +0800

    Donessssss

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
+var list = document.querySelector('ol');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
 $(document).ready(function(){
   $("#randTask").click(function(){
-      $("#listTask li").remove();
+    $("#listTask li").remove();   
       $.ajax({
           url:"https://jsonplaceholder.typicode.com/todos",
           datatype:"json",
           success: function(data){
               for(let x = 0; x < 10; x++){
                   let pick = Math.floor(Math.random()*data.length);
-                  $("#listTask").append("<li style='color:white'> " +data[pick].title+" </li>");
+                  $("#listTask").append("<li style='color:black'> " +data[pick].title+" </li>");
   
               }
              
@@ -17,3 +24,30 @@ $(document).ready(function(){
   });
 
 });
+
+
+$(document).ready(
+    function(){
+        $('#AddTask').click(
+            function(){
+                var toAdd = $('input[name=list_item]').val();
+                 $('ol').append('<li>' + toAdd + '</li>');
+            });
+       
+       $("input[name=ListItem]").keyup(function(event){
+          if(event.keyCode == 13){
+            $("#AddTask").click();
+          }         
+      });
+      
+      $(document).on('dblclick','li', function(){
+        $(this).toggleClass('strike').fadeOut('slow');    
+      });
+      
+      $('input').focus(function() {
+        $(this).val('');
+      });
+      
+    //   $('ol').sortable();
+
+    });
